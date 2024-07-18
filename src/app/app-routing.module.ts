bdae15525c2e391c2bd18d9f193363bdc60b295e
josue <josue@tec-software.com>
2024-07-17 21:18:48 -0300

    ADD, router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,34 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './features/auth/login/login.component';
+import { DashboardComponent } from './features/dashboard/dashboard.component';
+import { CoursesComponent } from './features/dashboard/courses/courses.component';
+import { EnrollmentsComponent } from './features/dashboard/enrollments/enrollments.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'auth',
+    component: LoginComponent,
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+    children: [
+      {
+        // /dashboard/courses
+        path: 'courses',
+        component: CoursesComponent,
+      },
+      {
+        path: 'enrollments',
+        component: EnrollmentsComponent,
+      },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
